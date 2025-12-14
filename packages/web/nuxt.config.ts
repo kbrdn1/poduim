abfffc6be90ad60c88763d2a9a4b238dd580348c
Kylian Bardini <kylianb1@icloud.com>
2025-12-14 15:46:05 +0100

    🔧 chore(web): update nuxt and tailwind configuration

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api/v1",
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:3333/api/v1",
     },
   },
 
